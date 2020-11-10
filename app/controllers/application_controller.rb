3573b182ce75abad5939b2402021adfac3086be4
Hoan Nguyen <hoannguyen@vncdevs.io>
2020-11-10 11:29:07 +0700

    Implement user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  layout :layout_by_resource
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def layout_by_resource
+    if user_signed_in? && current_user.admin?
+      'application_admin'
+    else
+      'application'
+    end
+  end
 end
