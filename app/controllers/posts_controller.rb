a6ea21790dce1c02a222e87559f8cfc568844849
Hoan Nguyen <hoannguyen@vncdevs.io>
2020-11-11 13:15:41 +0700

    Add layout for regular user and refactor code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.includes(:user).order(created_at: :desc)
     authorize @posts
   end
 
